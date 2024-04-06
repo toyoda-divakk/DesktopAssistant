95735067f85e477eeb05fe1b864bb2d4339ee557
genkokudo <y-toyoda@live.jp>
2024-04-07 07:55:31 +0900

    ちょっと進める

---
+++ DesktopAssistant.Core/Models/TodoTask.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopAssistant.Core.Models;
+
+/// <summary>
+/// Todoタスク
+/// </summary>
+public record TodoTask
+{
+    /// <summary>
+    /// タスクのID
+    /// </summary>
+    public Guid Id
+    {
+        get; init;
+    }
+
+    /// <summary>
+    /// タスクの内容
+    /// </summary>
+    public string Content
+    {
+        get; init;
+    }
+
+    /// <summary>
+    /// タスクが完了しているかどうか
+    /// </summary>
+    public bool IsDone
+    {
+        get; set;
+    }
+
+    /// <summary>
+    /// タスクの作成日時
+    /// </summary>
+    public DateTime CreatedAt
+    {
+        get; init;
+    }
+
+    /// <summary>
+    /// タスクの更新日時
+    /// </summary>
+    public DateTime UpdatedAt
+    {
+        get; set;
+    }
+
+    /// <summary>
+    /// タスクの期限
+    /// </summary>
+    public DateTime? Deadline
+    {
+        get; set;
+    }
+}
